31aae28b676ad92ff8b8aae92c2526ff96beae1b
Vishal Talwar <vtalwar.w@gmail.com>
2023-01-09 21:58:34 -0800

    adds distributedStore with leader writes

---
+++ internal/store/distributed.go
@@ -0,0 +1,348 @@
+package store
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"path/filepath"
+	"time"
+
+	raftboltdb "github.com/hashicorp/raft-boltdb"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/proto"
+
+	"github.com/hashicorp/raft"
+
+	api "github.com/comfforts/comff-stores/api/v1"
+
+	"github.com/comfforts/comff-stores/pkg/errors"
+	"github.com/comfforts/comff-stores/pkg/logging"
+	storeModels "github.com/comfforts/comff-stores/pkg/models/store"
+	"github.com/comfforts/comff-stores/pkg/services/log"
+	"github.com/comfforts/comff-stores/pkg/services/store"
+)
+
+const (
+	ERROR_CREATING_LOG_STORE_DIR  string = "error creating raft log store folder"
+	ERROR_CREATING_STABLE_STORE   string = "error creating raft stable store"
+	ERROR_CREATING_SNAPSHOT_STORE string = "error creating raft snapshot store"
+	ERROR_CREATING_RAFT           string = "error creating raft node"
+	ERROR_RAFT_JOIN_CONFIG        string = "error getting config  to join raft cluster"
+	ERROR_REMOVING_EXISTING       string = "error removing existing node"
+	ERROR_RAFT_JOIN               string = "error joining raft cluster"
+	ERROR_RAFT_STATE              string = "error identifying raft node state"
+	ERROR_BUF_WRITE               string = "error wrting to buffer"
+	ERROR_MARSHAL_PROTO           string = "error marshalling proto"
+	ERROR_RAFT_APPLY              string = "error apply raft command"
+	ERROR_RAFT_APPLY_RESP         string = "error apply raft command response"
+	ERROR_RAFT_TIMEOUT            string = "error: raft timed out"
+	ERROR_RAFT_SHUTDOWN           string = "error: raft shutdown"
+)
+
+var (
+	ErrRaftTimeout = errors.NewAppError(ERROR_RAFT_TIMEOUT)
+)
+
+type Config struct {
+	Raft struct {
+		raft.Config
+		BindAddr    string
+		StreamLayer *StreamLayer
+		Bootstrap   bool
+	}
+	log.Config
+	Logger *logging.AppLogger
+}
+
+type DistributedStores struct {
+	config Config
+	stores storeModels.Stores
+	raft   *raft.Raft
+}
+
+func NewDistributedStores(dataDir string, config Config) (*DistributedStores, error) {
+	config.Logger.Info("creating distributed store instance: ", zap.Any("node id", config.Raft.Config.LocalID))
+	l := &DistributedStores{
+		config: config,
+	}
+	l.setupStores(dataDir)
+	if err := l.setupRaft(dataDir); err != nil {
+		return nil, err
+	}
+	return l, nil
+}
+
+func (ds *DistributedStores) setupStores(dataDir string) {
+	ds.stores = store.NewStoreService(ds.config.Logger)
+}
+
+func (ds *DistributedStores) setupRaft(dataDir string) error {
+	fsm := &fsm{
+		StoreService: ds.stores,
+		logger:       ds.config.Logger,
+	}
+
+	ds.config.Logger.Info("creating raft log store folder")
+	logDir := filepath.Join(dataDir, "raft", "log")
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		ds.config.Logger.Error(ERROR_CREATING_LOG_STORE_DIR, zap.Error(err))
+		return errors.WrapError(err, ERROR_CREATING_LOG_STORE_DIR)
+	}
+	logConfig := ds.config
+	logConfig.Segment.InitialOffset = 1
+
+	ds.config.Logger.Info("creating raft log store")
+	logStore, err := newLogStore(logDir, logConfig)
+	if err != nil {
+		return err
+	}
+
+	ds.config.Logger.Info("creating raft stable store")
+	stableStore, err := raftboltdb.NewBoltStore(filepath.Join(dataDir, "raft", "stable"))
+	if err != nil {
+		ds.config.Logger.Error(ERROR_CREATING_STABLE_STORE, zap.Error(err))
+		return errors.WrapError(err, ERROR_CREATING_STABLE_STORE)
+	}
+
+	ds.config.Logger.Info("creating raft snapshot store")
+	retain := 1
+	snapshotStore, err := raft.NewFileSnapshotStore(
+		filepath.Join(dataDir, "raft"),
+		retain,
+		os.Stderr,
+	)
+	if err != nil {
+		ds.config.Logger.Error(ERROR_CREATING_SNAPSHOT_STORE, zap.Error(err))
+		return errors.WrapError(err, ERROR_CREATING_SNAPSHOT_STORE)
+	}
+
+	ds.config.Logger.Info("setup raft streamlayer transport")
+	maxPool := 5
+	timeout := 10 * time.Second
+	transport := raft.NewNetworkTransport(
+		ds.config.Raft.StreamLayer,
+		maxPool,
+		timeout,
+		os.Stderr,
+	)
+
+	ds.config.Logger.Info("setup raft config")
+	config := raft.DefaultConfig()
+	config.LocalID = ds.config.Raft.LocalID
+	if ds.config.Raft.HeartbeatTimeout != 0 {
+		config.HeartbeatTimeout = ds.config.Raft.HeartbeatTimeout
+	}
+	if ds.config.Raft.ElectionTimeout != 0 {
+		config.ElectionTimeout = ds.config.Raft.ElectionTimeout
+	}
+	if ds.config.Raft.LeaderLeaseTimeout != 0 {
+		config.LeaderLeaseTimeout = ds.config.Raft.LeaderLeaseTimeout
+	}
+	if ds.config.Raft.CommitTimeout != 0 {
+		config.CommitTimeout = ds.config.Raft.CommitTimeout
+	}
+
+	ds.config.Logger.Info("creating raft node")
+	ds.raft, err = raft.NewRaft(
+		config,
+		fsm,
+		logStore,
+		stableStore,
+		snapshotStore,
+		transport,
+	)
+	if err != nil {
+		ds.config.Logger.Error(ERROR_CREATING_RAFT, zap.Error(err))
+		return errors.WrapError(err, ERROR_CREATING_RAFT)
+	}
+
+	ds.config.Logger.Info("identifying raft node state")
+	hasState, err := raft.HasExistingState(
+		logStore,
+		stableStore,
+		snapshotStore,
+	)
+	if err != nil {
+		ds.config.Logger.Error(ERROR_RAFT_STATE, zap.Error(err))
+		return errors.WrapError(err, ERROR_RAFT_STATE)
+	}
+	ds.config.Logger.Info("raft node state", zap.Bool("is-bootstrap", ds.config.Raft.Bootstrap), zap.Bool("has-state", hasState), zap.String("bind-addr", ds.config.Raft.BindAddr), zap.Any("transport-addr", transport.LocalAddr()), zap.Any("raft-addr", raft.ServerAddress(ds.config.Raft.BindAddr)))
+	if ds.config.Raft.Bootstrap && !hasState {
+		config := raft.Configuration{
+			Servers: []raft.Server{{
+				ID:      config.LocalID,
+				Address: raft.ServerAddress(ds.config.Raft.BindAddr),
+			}},
+		}
+		ds.config.Logger.Info("bootstrapping raft cluster")
+		err = ds.raft.BootstrapCluster(config).Error()
+	}
+	return err
+}
+
+func (ds *DistributedStores) AddStore(ctx context.Context, s *storeModels.Store) (*storeModels.Store, error) {
+	ds.config.Logger.Info("distributed store AddStore()", zap.String("node", string(ds.config.Raft.LocalID)))
+	res, err := ds.apply(
+		AddStoreRequestType,
+		&api.AddStoreRequest{
+			Name:      s.Name,
+			Org:       s.Org,
+			City:      s.City,
+			Country:   s.Country,
+			Longitude: float32(s.Longitude),
+			Latitude:  float32(s.Latitude),
+			StoreId:   s.StoreId,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	store := storeModels.MapProtoToStore(res.(*api.AddStoreResponse).Store)
+	return store, nil
+}
+
+func (ds *DistributedStores) apply(reqType RequestType, req proto.Message) (
+	interface{},
+	error,
+) {
+	ds.config.Logger.Info("distributed store apply()", zap.String("node", string(ds.config.Raft.LocalID)))
+	var buf bytes.Buffer
+	_, err := buf.Write([]byte{byte(reqType)})
+	if err != nil {
+		ds.config.Logger.Error(ERROR_BUF_WRITE, zap.Error(err))
+		return nil, errors.WrapError(err, ERROR_BUF_WRITE)
+	}
+	b, err := proto.Marshal(req)
+	if err != nil {
+		ds.config.Logger.Error(ERROR_MARSHAL_PROTO, zap.Error(err))
+		return nil, errors.WrapError(err, ERROR_MARSHAL_PROTO)
+	}
+	_, err = buf.Write(b)
+	if err != nil {
+		ds.config.Logger.Error(ERROR_BUF_WRITE, zap.Error(err))
+		return nil, errors.WrapError(err, ERROR_BUF_WRITE)
+	}
+	timeout := 10 * time.Second
+	future := ds.raft.Apply(buf.Bytes(), timeout)
+	if future.Error() != nil {
+		ds.config.Logger.Error(ERROR_RAFT_APPLY, zap.Error(future.Error()))
+		return nil, future.Error()
+	}
+	res := future.Response()
+	if err, ok := res.(error); ok {
+		ds.config.Logger.Error(ERROR_RAFT_APPLY_RESP, zap.Error(err))
+		return nil, err
+	}
+	return res, nil
+}
+
+func (ds *DistributedStores) GetStore(ctx context.Context, id string) (*storeModels.Store, error) {
+	ds.config.Logger.Info("distributed store GetStore()", zap.String("node", string(ds.config.Raft.LocalID)))
+	return ds.stores.GetStore(ctx, id)
+}
+
+func (ds *DistributedStores) GetStoresForGeoPoint(ctx context.Context, lat, long float64, dist int) ([]*storeModels.StoreGeo, error) {
+	ds.config.Logger.Info("distributed store GetStoresForGeoPoint()", zap.String("node", string(ds.config.Raft.LocalID)))
+	return ds.stores.GetStoresForGeoPoint(ctx, lat, long, dist)
+}
+
+func (ds *DistributedStores) GetStoreStats() storeModels.StoreStats {
+	ds.config.Logger.Info("distributed store GetStoreStats()", zap.String("node", string(ds.config.Raft.LocalID)))
+	return ds.stores.GetStoreStats()
+}
+
+func (ds *DistributedStores) Join(id, addr string) error {
+	ds.config.Logger.Info("distributed store joining", zap.String("node", string(ds.config.Raft.LocalID)))
+	configFuture := ds.raft.GetConfiguration()
+	if err := configFuture.Error(); err != nil {
+		ds.config.Logger.Error(ERROR_RAFT_JOIN_CONFIG, zap.Error(err))
+		return err
+	}
+	serverID := raft.ServerID(id)
+	serverAddr := raft.ServerAddress(addr)
+	for _, srv := range configFuture.Configuration().Servers {
+		if srv.ID == serverID || srv.Address == serverAddr {
+			if srv.ID == serverID && srv.Address == serverAddr {
+				// server has already joined
+				return nil
+			}
+			// remove the existing server
+			removeFuture := ds.raft.RemoveServer(serverID, 0, 0)
+			if err := removeFuture.Error(); err != nil {
+				ds.config.Logger.Error(ERROR_REMOVING_EXISTING, zap.Error(err))
+				return err
+			}
+		}
+	}
+
+	addFuture := ds.raft.AddNonvoter(serverID, serverAddr, 0, 0)
+	if err := addFuture.Error(); err != nil {
+		ds.config.Logger.Error(ERROR_RAFT_JOIN, zap.Error(err))
+		return err
+	}
+	return nil
+}
+
+func (ds *DistributedStores) Leave(id string) error {
+	ds.config.Logger.Info("distributed store leaving", zap.String("node", string(raft.ServerID(id))))
+	removeFuture := ds.raft.RemoveServer(raft.ServerID(id), 0, 0)
+	return removeFuture.Error()
+}
+
+func (ds *DistributedStores) WaitForLeader(timeout time.Duration) error {
+	ds.config.Logger.Debug("distributed store waiting for leader", zap.String("node", string(ds.config.Raft.LocalID)))
+	timeoutc := time.After(timeout)
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-timeoutc:
+			ds.config.Logger.Error(ERROR_RAFT_TIMEOUT, zap.Error(ErrRaftTimeout))
+			return ErrRaftTimeout
+		case <-ticker.C:
+			if rLead := ds.raft.Leader(); rLead != "" {
+				ds.config.Logger.Debug("leader: ", zap.Any("leader", rLead))
+				return nil
+			}
+		}
+	}
+}
+
+func (ds *DistributedStores) Reader(ctx context.Context, filePath string) (*os.File, error) {
+	return ds.stores.Reader(ctx, filePath)
+}
+
+func (ds *DistributedStores) GetServers(ctx context.Context) ([]*api.Server, error) {
+	future := ds.raft.GetConfiguration()
+	if err := future.Error(); err != nil {
+		return nil, err
+	}
+	var servers []*api.Server
+	for _, server := range future.Configuration().Servers {
+		servers = append(servers, &api.Server{
+			Id:       string(server.ID),
+			RpcAddr:  string(server.Address),
+			IsLeader: ds.raft.Leader() == server.Address,
+		})
+	}
+	return servers, nil
+}
+
+func (ds *DistributedStores) ServerId() string {
+	return string(ds.config.Raft.LocalID)
+}
+
+func (ds *DistributedStores) Close() error {
+	ds.config.Logger.Info("closing distributed store", zap.String("node", string(ds.config.Raft.LocalID)))
+	f := ds.raft.Shutdown()
+	if err := f.Error(); err != nil {
+		ds.config.Logger.Error(ERROR_RAFT_SHUTDOWN, zap.Error(err))
+		return errors.WrapError(err, ERROR_RAFT_SHUTDOWN)
+	}
+	ds.stores.Close()
+	return nil
+}
+
+func (ds *DistributedStores) SetReady(ctx context.Context, ready bool) {
+	ds.stores.SetReady(ctx, ready)
+}
---
+++ internal/store/distributed_test.go
@@ -0,0 +1,187 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/raft"
+	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
+
+	"github.com/comfforts/comff-stores/pkg/logging"
+	testUtils "github.com/comfforts/comff-stores/pkg/utils/test"
+)
+
+const TEST_DIR = "test-data"
+
+func TestMultipleNodes(t *testing.T) {
+	var stores []*DistributedStores
+	nodeCount := 3
+	ports := dynaport.Get(nodeCount)
+
+	// remove data directory after test ends
+	defer func() {
+		err := os.RemoveAll(TEST_DIR)
+		require.NoError(t, err)
+	}()
+
+	getConfig := func(ln net.Listener, dataDir string, id int) Config {
+		config := Config{}
+		config.Raft.StreamLayer = NewStreamLayer(ln, nil, nil)
+		config.Raft.LocalID = raft.ServerID(fmt.Sprintf("%d", id))
+		config.Raft.HeartbeatTimeout = 500 * time.Millisecond
+		config.Raft.ElectionTimeout = 500 * time.Millisecond
+		config.Raft.LeaderLeaseTimeout = 500 * time.Millisecond
+		config.Raft.CommitTimeout = 5 * time.Millisecond
+		config.Segment.MaxIndexSize = 10
+		config.Raft.BindAddr = ln.Addr().String()
+
+		config.Logger = logging.NewTestAppLogger(dataDir)
+
+		if id == 0 {
+			config.Raft.Bootstrap = true
+		}
+		return config
+	}
+
+	// configure distributed store nodes
+	for i := 0; i < nodeCount; i++ {
+		// setup data directory
+		dataDir := filepath.Join(TEST_DIR, fmt.Sprintf("node-%d/", i))
+		err := os.MkdirAll(dataDir, os.ModePerm)
+		require.NoError(t, err)
+
+		ln, err := net.Listen(
+			"tcp",
+			fmt.Sprintf("127.0.0.1:%d", ports[i]),
+		)
+		require.NoError(t, err)
+
+		config := getConfig(ln, dataDir, i)
+
+		ds, err := NewDistributedStores(dataDir, config)
+		require.NoError(t, err)
+
+		if i != 0 {
+			// join node with cluster
+			err = stores[0].Join(fmt.Sprintf("%d", i), ln.Addr().String())
+			require.NoError(t, err)
+		} else {
+			// bootstrap cluster with first node
+			err = ds.WaitForLeader(3 * time.Second)
+			require.NoError(t, err)
+		}
+
+		stores = append(stores, ds)
+	}
+	// skip the cycle and wait for bit
+	time.Sleep(50 * time.Millisecond)
+
+	ctx := context.Background()
+	servers, err := stores[0].GetServers(ctx)
+	require.NoError(t, err)
+	require.Equal(t, len(servers), 3)
+	require.Equal(t, servers[0].IsLeader, true)
+	require.Equal(t, servers[1].IsLeader, false)
+	require.Equal(t, servers[2].IsLeader, false)
+
+	recs := testUtils.CreateStoreModelList()
+
+	for _, v := range recs {
+		// add a store through leader node
+		ctx := context.Background()
+		st, err := stores[0].AddStore(ctx, v)
+		require.NoError(t, err)
+
+		// skip the cycle and wait for bit
+		time.Sleep(50 * time.Millisecond)
+
+		// verify all nodes nodes have data synced
+		for j := 0; j < len(stores); j++ {
+			ctx := context.Background()
+			got, err := stores[j].GetStore(ctx, st.ID)
+			require.NoError(t, err)
+			require.Equal(t, st.ID, got.ID)
+			require.Equal(t, st.Name, got.Name)
+		}
+	}
+
+	// remove a follower node
+	err = stores[0].Leave(stores[1].ServerId())
+	require.NoError(t, err)
+
+	// skip the cycle and wait for bit
+	time.Sleep(50 * time.Millisecond)
+
+	ctx = context.Background()
+	servers, err = stores[0].GetServers(ctx)
+	require.NoError(t, err)
+	require.Equal(t, len(servers), 2)
+	require.Equal(t, servers[0].IsLeader, true)
+	require.Equal(t, servers[1].IsLeader, false)
+
+	// skip the cycle and wait for bit
+	time.Sleep(50 * time.Millisecond)
+
+	v := testUtils.CreateStoreModel()
+
+	// adding a store through follower node should fail
+	ctx = context.Background()
+	_, err = stores[2].AddStore(ctx, v)
+	require.NotNil(t, err)
+
+	// add a new store through leader node
+	stn, err := stores[0].AddStore(ctx, v)
+	require.NoError(t, err)
+
+	// getting new store through removed follower node should fail
+	gt, err := stores[1].GetStore(ctx, stn.ID)
+	require.NotNil(t, err)
+	require.Nil(t, gt)
+
+	time.Sleep(50 * time.Millisecond)
+
+	// getting new store through active follower node should succeed
+	got, err := stores[2].GetStore(ctx, stn.ID)
+	require.NoError(t, err)
+	require.Equal(t, stn.ID, got.ID)
+
+	// join a new to cluster
+	dataDir := filepath.Join(TEST_DIR, fmt.Sprintf("node-%d/", 3))
+	err = os.MkdirAll(dataDir, os.ModePerm)
+	require.NoError(t, err)
+
+	newPorts := dynaport.Get(1)
+	ln, err := net.Listen(
+		"tcp",
+		fmt.Sprintf("127.0.0.1:%d", newPorts[0]),
+	)
+	require.NoError(t, err)
+
+	config := getConfig(ln, dataDir, 3)
+	ds, err := NewDistributedStores(dataDir, config)
+	require.NoError(t, err)
+
+	err = stores[0].Join(fmt.Sprintf("%d", 3), ln.Addr().String())
+	require.NoError(t, err)
+
+	stores = append(stores, ds)
+
+	time.Sleep(50 * time.Millisecond)
+
+	// TODO getting new store through new follower node should succeed
+	got, err = stores[3].GetStore(ctx, stn.ID)
+	require.NoError(t, err)
+	require.Equal(t, stn.ID, got.ID)
+
+	// shutdown all nodes
+	for j := 0; j < len(stores); j++ {
+		err := stores[j].Close()
+		require.NoError(t, err)
+	}
+}
