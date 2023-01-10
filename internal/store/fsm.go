aebb8ec2922e78c9466d4b0c22e9e0e975c11b75
Vishal Talwar <vtalwar.w@gmail.com>
2023-01-09 21:55:50 -0800

    adds raft FSM, LgSt, StSt, SnShSt and tport impls

---
+++ internal/store/fsm.go
@@ -0,0 +1,114 @@
+package store
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"io"
+
+	api "github.com/comfforts/comff-stores/api/v1"
+	"github.com/comfforts/comff-stores/pkg/logging"
+	"github.com/hashicorp/raft"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/proto"
+
+	fileModels "github.com/comfforts/comff-stores/pkg/models/file"
+	storeModels "github.com/comfforts/comff-stores/pkg/models/store"
+	fileUtils "github.com/comfforts/comff-stores/pkg/utils/file"
+)
+
+var _ raft.FSM = (*fsm)(nil)
+
+type fsm struct {
+	StoreService storeModels.Stores
+	logger       *logging.AppLogger
+}
+
+type RequestType uint8
+
+const (
+	AddStoreRequestType    RequestType = 0
+	StoreUploadRequestType RequestType = 1
+)
+
+func (fs *fsm) Apply(record *raft.Log) interface{} {
+	buf := record.Data
+	reqType := RequestType(buf[0])
+	switch reqType {
+	case AddStoreRequestType:
+		return fs.applyAddStore(buf[1:])
+	}
+	return nil
+}
+
+func (fs *fsm) applyAddStore(b []byte) interface{} {
+	var req api.AddStoreRequest
+	err := proto.Unmarshal(b, &req)
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+	store, err := fs.StoreService.AddStore(ctx, storeModels.MapStoreRequestToStore(&req))
+	if err != nil {
+		return err
+	}
+
+	return &api.AddStoreResponse{
+		Ok:    true,
+		Store: storeModels.MapStoreModelToResponse(store),
+	}
+}
+
+func (fs *fsm) Snapshot() (raft.FSMSnapshot, error) {
+	ctx := context.Background()
+	r, err := fs.StoreService.Reader(ctx, "")
+	if err != nil {
+		return nil, err
+	}
+	return &snapshot{
+		reader: r,
+		logger: fs.logger,
+	}, nil
+}
+
+func (fs *fsm) Restore(rf io.ReadCloser) error {
+	ctx := context.Background()
+
+	r := bufio.NewReader(rf)
+	dec := json.NewDecoder(r)
+
+	// read open bracket
+	t, err := dec.Token()
+	if err != nil || t != json.Delim('[') {
+		fs.logger.Error("start token error", zap.Error(err))
+		return fileUtils.ErrStartToken
+	}
+
+	// while the array contains values
+	for dec.More() {
+		var result fileModels.JSONMapper
+		err := dec.Decode(&result)
+		if err != nil {
+			fs.logger.Error(fileUtils.ERROR_DECODING_RESULT, zap.Error(err))
+		} else {
+			store, err := storeModels.MapResultToStore(result)
+			if err != nil {
+				fs.logger.Error("error processing store data", zap.Error(err), zap.Any("storeJson", r))
+			}
+
+			addedStr, err := fs.StoreService.AddStore(ctx, store)
+			if addedStr == nil || err != nil {
+				fs.logger.Error("error processing store data", zap.Error(err), zap.Any("store", store))
+			}
+		}
+	}
+
+	// read closing bracket
+	t, err = dec.Token()
+	if err != nil || t != json.Delim(']') {
+		fs.logger.Error("end token error", zap.Error(err))
+		return fileUtils.ErrEndToken
+	}
+	return nil
+}
---
+++ internal/store/logstore.go
@@ -0,0 +1,81 @@
+package store
+
+import (
+	api "github.com/comfforts/comff-stores/api/v1"
+	"github.com/comfforts/comff-stores/pkg/errors"
+	"github.com/comfforts/comff-stores/pkg/logging"
+	"github.com/comfforts/comff-stores/pkg/services/log"
+	"github.com/hashicorp/raft"
+	"go.uber.org/zap"
+)
+
+var _ raft.LogStore = (*logStore)(nil)
+
+const (
+	ERROR_CREATING_LOGSTORE     string = "error creating raft log store"
+	ERROR_GETTING_LOGSTORE_LOGS string = "error getting raft logstore logs"
+	ERROR_STORING_LOGSTORE_LOGS string = "error storing raft logstore logs"
+)
+
+type logStore struct {
+	*log.Recorder
+	logger *logging.AppLogger
+}
+
+func newLogStore(dir string, c Config) (*logStore, error) {
+	log, err := log.NewRecorder(dir, c.Config)
+	if err != nil {
+		c.Logger.Error(ERROR_CREATING_LOGSTORE, zap.Error(err))
+		return nil, errors.WrapError(err, ERROR_CREATING_LOGSTORE)
+	}
+	return &logStore{log, c.Logger}, nil
+}
+
+func (l *logStore) FirstIndex() (uint64, error) {
+	lowest, err := l.LowestOffset()
+	l.logger.Debug("lowest offset", zap.Uint64("lowest", lowest), zap.Error(err))
+	return lowest, err
+}
+
+func (l *logStore) LastIndex() (uint64, error) {
+	off, err := l.HighestOffset()
+	l.logger.Debug("highest offset", zap.Uint64("highest", off), zap.Error(err))
+	return off, err
+}
+
+func (l *logStore) GetLog(index uint64, out *raft.Log) error {
+	in, err := l.Read(index)
+	if err != nil {
+		l.logger.Error(ERROR_GETTING_LOGSTORE_LOGS, zap.Error(err))
+		return errors.WrapError(err, ERROR_GETTING_LOGSTORE_LOGS)
+	}
+	out.Data = in.Value
+	out.Index = in.Offset
+	out.Type = raft.LogType(in.Type)
+	out.Term = in.Term
+	l.logger.Debug("got log record", zap.String("value", string(in.Value)), zap.Uint64("offset", in.Offset))
+	return nil
+}
+
+func (l *logStore) StoreLog(record *raft.Log) error {
+	l.logger.Debug("storing log record", zap.String("value", string(record.Data)))
+	return l.StoreLogs([]*raft.Log{record})
+}
+
+func (l *logStore) StoreLogs(records []*raft.Log) error {
+	for _, record := range records {
+		if _, err := l.Append(&api.Record{
+			Value: record.Data,
+			Term:  record.Term,
+			Type:  uint32(record.Type),
+		}); err != nil {
+			l.logger.Error(ERROR_STORING_LOGSTORE_LOGS, zap.Error(err))
+			return errors.WrapError(err, ERROR_STORING_LOGSTORE_LOGS)
+		}
+	}
+	return nil
+}
+
+func (l *logStore) DeleteRange(min, max uint64) error {
+	return l.Truncate(max)
+}
